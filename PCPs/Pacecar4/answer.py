fad5b31b22d84b50a1e4e355a0e9d4b519b53a1f
Malachi Parks <Malachiparks1999@gmail.com>
2022-02-17 21:54:44 -0500

    Working on python file to crack password

---
+++ PCPs/Pacecar4/answer.py
@@ -0,0 +1,11 @@
+# Variables
+junkString="nhlie~q4dj:fiRz?Oc!e'{qj"
+i=0
+answerString=""
+# Original Junk String
+print("Original Junk String: nhlie~q4dj:fiRz?Oc!e'{qj")
+
+# XORing junk string piece by piece
+for letter in junkString:
+    letter ^ i
+    
