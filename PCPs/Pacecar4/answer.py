8a3dd66bdee2f29265a7cea4dc38e1f19c059d4e
Malachi Parks <Malachiparks1999@gmail.com>
2022-02-17 23:06:51 -0500

    Modifying answer.py to take each letter, find anscii value then xor

--- PCPs/Pacecar4/answer.py
+++ PCPs/Pacecar4/answer.py
@@ -5,7 +5,11 @@ answerString=""
 # Original Junk String
 print("Original Junk String: nhlie~q4dj:fiRz?Oc!e'{qj")
 
+'''
 # XORing junk string piece by piece
 for letter in junkString:
-    letter ^ i
-    
+    letterNum = int(letter)
+    flagLetter =  letterNum ^ i
+    answerString += flagLetter
+'''    
+print("Flag: " + answerString)
