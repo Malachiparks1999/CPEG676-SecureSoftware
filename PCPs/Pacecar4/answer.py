82801e1903e521a909a2c7376563b7d2e41b721d
Malachi Parks <Malachiparks1999@gmail.com>
2022-02-18 02:00:33 -0500

    Now prints out the flag with the answer

--- PCPs/Pacecar4/answer.py
+++ PCPs/Pacecar4/answer.py
@@ -2,14 +2,14 @@
 junkString="nhlie~q4dj:fiRz?Oc!e'{qj"
 i=0
 answerString=""
+
 # Original Junk String
 print("Original Junk String: nhlie~q4dj:fiRz?Oc!e'{qj")
 
-'''
 # XORing junk string piece by piece
 for letter in junkString:
-    letterNum = int(letter)
-    flagLetter =  letterNum ^ i
-    answerString += flagLetter
-'''    
+    flagLetter = ord(letter)^i
+    answerString += chr(flagLetter)
+    i += 1
+    
 print("Flag: " + answerString)
