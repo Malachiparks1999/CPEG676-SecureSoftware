5b0413f944a2ba5a67d431cd6b7e050d8ed8a573
Malachi Parks <Malachiparks1999@gmail.com>
2022-02-19 16:15:56 -0500

    Halfway through without the sequence in place

--- PCPs/Pacecar5/Mparks_CrackmeV1.c
+++ PCPs/Pacecar5/Mparks_CrackmeV1.c
@@ -4,3 +4,23 @@ Section:        CPEG476-010
 Assignment:     Pacecar Problem 5
 File Description:  Creating a problem which takes in random junk and produces a flag
 */
+#include <stdlib.h>
+#include <stdio.h>
+#define MAX 26
+
+int main(){
+    // set up variables
+    int term1 = 0, term2 = 1, term3 = term1 + term2;
+    int flagLen = MAX;
+    char userInput[MAX];
+
+    // ask for user input
+    printf("Enter the secret:\n");
+
+    // loop and check
+    for(int i=0; i <= MAX; i++){
+
+    }
+
+    return 0;
+}
