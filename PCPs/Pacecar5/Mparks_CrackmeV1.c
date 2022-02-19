97a310700665e0fbcd0b75f4730f2375a8ab1b2b
Malachi Parks <Malachiparks1999@gmail.com>
2022-02-19 17:15:11 -0500

    Finished crackme and changed up the flag ideas

--- PCPs/Pacecar5/Mparks_CrackmeV1.c
+++ PCPs/Pacecar5/Mparks_CrackmeV1.c
@@ -6,20 +6,31 @@ File Description:  Creating a problem which takes in random junk and produces a 
 */
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX 26
+#define MAX 20
 
 int main(){
     // set up variables
     int term1 = 0, term2 = 1, term3 = term1 + term2;
     int flagLen = MAX;
     char userInput[MAX];
+    char secret[MAX] = "\x55\x45\x47\x4e\x42\x42\x73\x4b\x24\x40\x16\x06\x51\x04\x1f\x39\x30\x7a\x5a\x08";
 
     // ask for user input
     printf("Enter the secret:\n");
+    fgets(userInput, MAX, stdin);
 
     // loop and check
     for(int i=0; i <= MAX; i++){
-
+        if(userInput[i]^term1%127 == secret[i] && i != MAX){
+            term1=term2;
+            term2=term3;
+            term3=term1+term2;
+        } else if(userInput[i]^term1%127 == secret[i] && i != MAX){
+            printf("That's the secret!");
+        } else {
+            printf("That's not my secret!");
+            return 0;
+        }
     }
 
     return 0;
