de81bf8c80fd3bf11d3b5c8dda9a17afe5875096
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-01 21:29:44 -0500

    First portion of project done?

--- Project1/MParks_32bit.asm
+++ Project1/MParks_32bit.asm
@@ -3,18 +3,44 @@
 ;;  Assignment:     Project 1
 ;;  File Description:  Used to print to the nc session
 
+;; Compiling instructions
+;; nasm -f elf MParks_32bit.asm 
+;; ld -m elf_i386 -s -o MParks_32bit MParks_32bit.o
+
 section .text:
-  global _start ;;to compile nasm -f elf t32.asm THEN ld -m elf_i386 -s -o run32 t32.o
+  global _start ;; tells the linker where to start
 
 util:
   ;; this line and above set up new stack frame
   push ebp
   mov ebp, esp
+
   ;; System call to print out to stdout my msg
-  sub esp, 39   ;; where message is stored
+  sub esp, 41   ;; where message is stored
   mov dword [esp], msg  ;; load in message
-  mov edx, 39   ;; msg length
-  mov ecx, [ebp-39]  ;; load in message
+  mov edx, 41   ;; msg length
+  mov ecx, [ebp-41]  ;; load in message
+  mov ebx, 1    ;; write to stdout (file descripter 1)
+  mov eax, 4    ;; call sys_write
+  int 0x80      ;; sys_call
+
+  ;; print out multiplication message
+  sub esp, 23
+  mov dword [esp], mul  ;; load in message
+  mov edx, 23   ;; msg length
+  mov ecx, [ebp-64]  ;; load in message
+  mov ebx, 1    ;; write to stdout (file descripter 1)
+  mov eax, 4    ;; call sys_write
+  int 0x80      ;; sys_call
+
+  ;; Variables for for printing
+  sub esp, 4
+  mov dword [ebp-68], 11    ;; first num to multiply
+  imul ecx, [ebp-68], 7    ;; literal number 77
+  sub ecx, 22
+
+  ;; print out multiplication result
+  mov edx, 2   ;; msg length\
   mov ebx, 1    ;; write to stdout (file descripter 1)
   mov eax, 4    ;; call sys_write
   int 0x80      ;; sys_call
@@ -29,5 +55,9 @@ _start:
   mov eax, 0x01   ;; system call arg number
   int 0x80        ;; system call interupt
 
-section	.data
-  msg: db 'I will be doing some simple math today!', 39 ;; will be using this to show whats happening
+section	.data ;;0xA and 0xD are new line and then carriage return (enter)
+  msg: db 'I will be doing some simple math today!', 0xA, 0xD ;; will be using this to show whats happening
+  mul: db 'Multiplying 7 * 11: 77', 0xA, 0xD ;; will be used in showing multiplication
+
+segment .bss
+  res resb 1
