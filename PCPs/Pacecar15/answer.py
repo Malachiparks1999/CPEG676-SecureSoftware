6e96e440638960160c24d0e3f3ce1e845a8b1ce3
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-16 16:53:03 -0400

    Setting up PCP15, Flag Leak

---
+++ PCPs/Pacecar15/answer.py
@@ -0,0 +1,15 @@
+'''
+Creators:       Malachi Parks
+Section:        CPEG476-010
+Assignment:     PCP15: Flag Leak
+File Description:  Solving "Flag Leak" from PICO 2022
+
+Checksec info:
+
+
+File info:
+
+Radare2:
+afl --> list all functions
+
+'''
