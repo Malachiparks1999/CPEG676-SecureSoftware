625127914a397bca2df8dab52acd7645e020c9b5
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-17 14:54:28 -0400

    Fixing answer.py in chall_08 description, setup for 9

--- Project2/chall_08/answer.py
+++ Project2/chall_08/answer.py
@@ -2,7 +2,7 @@
 Creators:       Malachi Parks
 Section:        CPEG476-010
 Assignment:     Project 2 --- chall_08
-File Description:  Shellcode only since no other functions
+File Description:  First Write What Where (IMO Should be named Where What Write due to format)
 
 Checksec info:
 RELRO           STACK CANARY      NX            PIE             RPATH      RUNPATH      Symbols         FORTIFY Fortified       Fortifiable     FILE
---
+++ Project2/chall_09/answer.py
@@ -0,0 +1,17 @@
+'''
+Creators:       Malachi Parks
+Section:        CPEG476-010
+Assignment:     Project 2 --- chall_09
+File Description:  Reversing problem to figure out what X'ORd to pop win func
+
+Checksec info:
+
+File info:
+
+Radare2:
+afl --> list all functions
+'''
+
+# Import Libaries
+from pwn import *
+
