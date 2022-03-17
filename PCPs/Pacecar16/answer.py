aa6e22faaffd016a12e3d4988f42ee9406ba77f0
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-16 20:02:51 -0400

    Setup for PCP16

---
+++ PCPs/Pacecar16/answer.py
@@ -0,0 +1,21 @@
+'''
+Creators:       Malachi Parks
+Section:        CPEG476-010
+Assignment:     PCP16: 
+File Description:  
+
+Checksec info:
+
+
+File info:
+
+
+Radare2:
+afl --> list all functions
+
+nc 165.22.46.243 8877
+'''
+
+# Import Libraries
+from pwn import *
+
